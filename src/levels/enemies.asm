95f211b89398ded0b54a6bb1b2e3c289a5d18310
TakuikaNinja <graingtaku@myvuw.ac.nz>
2023-01-03 13:23:46 +1300

    Make wording of warp changes more consistent

--- src/levels/enemies.asm
+++ src/levels/enemies.asm
@@ -44,7 +44,7 @@ E_CastleArea6:
 	.db $0f, $03, $8e, $65, $e1, $bb, $38, $6d, $a8, $3e, $e5, $e7
 	.db $0f, $08, $0b, $02, $2b, $02, $5e, $65, $e1, $bb, $0e
 	.db $db, $0e, $bb, $8e, $db, $0e, $fe, $65, $ec, $0f, $0d
-	.db $4e, $65, $e1, $0f, $0e, $8e, $02, $e0, $0f, $10, $fe, $e5, $e1 ; move cheep-cheep room pipe change to prevent wrong warp
+	.db $4e, $65, $e1, $0f, $0e, $8e, $02, $e0, $0f, $10, $fe, $e5, $e1 ; move cheep-cheep room warp change to prevent wrong warp
 	.db $1b, $85, $7b, $0c, $5b, $95, $78, $2d, $90, $b5
 	.db $ff
 
