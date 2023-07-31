c36d0bac7c3611e88ee814d9231a0730d316e374
TakuikaNinja <takuikaninja@gmail.com>
2023-07-31 22:26:14 +1200

    Clear injury timer on time up

--- src/prg.asm
+++ src/prg.asm
@@ -6717,6 +6717,7 @@ ResGTCtrl:
 TimeUpOn:
 		sta PlayerStatus								; init player status (note A will always be zero here)
 		sta StarInvincibleTimer							; SM clear star timer
+		sta InjuryTimer									; SM clear injured invincibility timer
 		jsr GetPlayerColors								; update the colors (palette fix)
 		inc GameTimerExpiredFlag						; set game timer expiration flag
 		jmp ForceInjury									; do sub to kill the player (note player is small here)
