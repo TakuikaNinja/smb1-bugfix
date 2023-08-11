86ed3a239d820b0bd7e15a88ddb94ed49a628732
TakuikaNinja <takuikaninja@gmail.com>
2023-08-11 15:56:22 +1200

    Optimise PlayerVictoryWalk

--- src/prg.asm
+++ src/prg.asm
@@ -699,52 +699,42 @@ SetupVictoryMode:
 
 		lda #EndOfCastleMusic
 		sta EventMusicQueue								; play win castle music
+		lda #$00
+		sta ScrollLock
 		jmp IncModeTask_A								; jump to set next major task in victory mode
 
 ; -------------------------------------------------------------------------------------
 
 PlayerVictoryWalk:
-		ldy #$00										; set value here to not walk player by default
-		sty VictoryWalkControl
+		ldy #$00										; load value here to not walk player by default
 
-		lda Player_PageLoc								; get player's page location
+		lda Player_PageLoc								; get page location of player
 		cmp DestinationPageLoc							; compare with destination page location
 		bne PerformWalk									; if page locations don't match, branch
 
 		lda Player_X_Position							; otherwise get player's horizontal position
-		cmp #$60										; compare with preset horizontal position
+		cmp #$57										; compare with preset horizontal position
 		bcs DontWalk									; if still on other page, branch ahead
 
 PerformWalk:
-		inc VictoryWalkControl							; otherwise increment value and Y
-		iny												; note Y will be used to walk the player
+		iny												; otherwise increment Y to walk the player
 
 DontWalk:
 		tya												; put contents of Y in A and
 		jsr AutoControlPlayer							; use A to move player to the right or not
+		jsr UpdScrollVar								; do sub to update screen and scroll variables
 
 		lda ScreenLeft_PageLoc							; check page location of left side of screen
 		cmp DestinationPageLoc							; against set value here
-		beq ExitVWalk									; branch if equal to change modes if necessary
-
-		lda ScrollFractional
-		clc												; do fixed point math on fractional part of scroll
-		adc #$80
-		sta ScrollFractional							; save fractional movement amount
-
-		lda #$01										; set 1 pixel per frame
-		adc #$00										; add carry from previous addition
-		tay												; use as scroll amount
-
-		jsr ScrollScreen								; do sub to scroll the screen
-		jsr UpdScrollVar								; do another sub to update screen and scroll variables
-
-		inc VictoryWalkControl							; increment value to stay in this routine
+		bne ExitVWalk									; branch to leave if not equal
+		
+		lda #$01										; otherwise set scroll lock
+		sta ScrollLock
+		
+		lda Player_X_Speed								; check player's horizontal speed
+		beq IncModeTask_A								; if zero, branch to change modes
 
 ExitVWalk:
-		lda VictoryWalkControl							; load value set here
-		beq IncModeTask_A								; if zero, branch to change modes
-		
 		rts												; otherwise leave
 
 ; -------------------------------------------------------------------------------------
